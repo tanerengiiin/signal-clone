55548a2df6685b7f2b9fc378db45a53209acf2bb
tanerengiiin <tanerengin4444@gmail.com>
2024-08-18 13:51:07 +0300

    Fix build errors

--- app/(project)/layout.tsx
+++ app/(project)/layout.tsx
@@ -14,10 +14,8 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-  modal,
 }: Readonly<{
   children: React.ReactNode;
-  modal: React.ReactNode;
 }>) {
   return (
     <html lang="en">
@@ -33,7 +31,6 @@ export default function RootLayout({
               <Sidebar />
               <div className="flex-1">{children}</div>
             </main>
-            {modal}
           </LayoutClient>
         </ThemeProvider>
       </body>
--- components/sidebar/SidebarProfileButton.tsx
+++ components/sidebar/SidebarProfileButton.tsx
@@ -1,21 +1,27 @@
 "use client";
-import Link from "next/link";
 import React from "react";
 import SidebarButton from "./SidebarButton";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 import { useSession } from "next-auth/react";
+import { DialogTrigger, Dialog, DialogContent } from "../ui/dialog";
+import Profile from "../Profile";
 
 const SidebarProfileButton = () => {
   const { data: session } = useSession();
   return (
-    <Link href="/profile">
-      <SidebarButton tooltip="Profile">
-        <Avatar className="w-6 h-6">
-          <AvatarImage src={session?.user?.image || ''} />
-          <AvatarFallback className="text-[10px]">CN</AvatarFallback>
-        </Avatar>
-      </SidebarButton>
-    </Link>
+    <Dialog>
+      <DialogTrigger asChild>
+        <SidebarButton tooltip="Profile">
+          <Avatar className="w-6 h-6">
+            <AvatarImage src={session?.user?.image || ''} />
+            <AvatarFallback className="text-[10px]">CN</AvatarFallback>
+          </Avatar>
+        </SidebarButton>
+      </DialogTrigger>
+      <DialogContent className="w-[22rem] p-4 !rounded-lg">
+        <Profile/>
+      </DialogContent>
+    </Dialog>
   );
 };
 
